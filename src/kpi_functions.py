cd4baf8a8355b16a6a233846525f66cbd89e5971
Yvonne Akinyi Amugaga <amugagayvonne@gmail.com>
2026-02-05 21:46:05 +0100

    Add KPI calculation functions

---
+++ src/kpi_functions.py
@@ -0,0 +1,8 @@
+def current_ratio(current_assets, current_liabilities):
+    return current_assets / current_liabilities
+
+def debt_to_equity(total_liabilities, total_equity):
+    return total_liabilities / total_equity
+
+def return_on_assets(net_income, total_assets):
+    return net_income / total_assets
